fb2d49a626a401d8cef8a46319b5f41125c3c4fb
Andy Dao <andydao2020@gmail.com>
2024-05-05 14:17:22 -0500

    update to deploy

--- digital-portfolio/src/App.tsx
+++ digital-portfolio/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from "./Home/Home";
 import Resume from "./Resume/Resume";
 import Projects from "./Projects/Projects";
@@ -9,20 +9,24 @@ import SideNavigationBar from "./Global-Elements/SideNavigationBar";
 import ProjectRoute from "./Projects/ProjectInDepth/ProjectRoute";
 import BackToTopBtn from "./Global-Elements/BackToTopBtn";
 
-const App: React.FC = () => (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <div id={"sideBar"}>
-              <SideNavigationBar />
-          </div>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Projects/:project" element={<ProjectRoute/>} />
-            <Route path="/Projects" element={<Projects />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/Resume" element={<Resume />} />
-        </Routes>
-          <BackToTopBtn />
-      </BrowserRouter>
-  );
+const App: React.FC = () => {
+    console.log("Got here...");
+    console.log(process.env.PUBLIC_URL);
+    return (
+        <HashRouter>
+            <div id={"sideBar"}>
+                <SideNavigationBar/>
+            </div>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/Projects/:project" element={<ProjectRoute/>}/>
+                <Route path="/Projects" element={<Projects/>}/>
+                <Route path="/Contact" element={<Contact/>}/>
+                <Route path="/Resume" element={<Resume/>}/>
+            </Routes>
+            <BackToTopBtn/>
+        </HashRouter>
+    );
+}
 
 export default App;
